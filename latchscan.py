fb6cb6bd1cb0f9f0df2afa2cac9efaee209f805a
Andrew Mike <logomancer@gmail.com>
2016-09-17 01:38:10 -0400

    Added email functionality; fixed minor bug in username regex capture

--- latchscan.py
+++ latchscan.py
@@ -18,18 +18,29 @@
 
 import sys
 import re
+import smtplib
 from datetime import datetime,date
+from email.mime.text import MIMEText
 
 
 filename = "latchburg.log"
 date_format = "%Y-%m-%d"
+
 # This is the regex for the logfile. Breaking it down:
 # (\d{4}-\d{2}-\d{2})) matches the date
 # (\d{2}:\d{2}:\d{2}) matches the time (we leave the milliseconds out of the capture group because we don't need that resolution)
 # ([AU]) matches the first character in "Allowed" or "Unauthorized" in order to see whether entry was allowed or denied.
 # Since card numbers of denied persons are logged, and that may contain sensitive info, it's important that that not go out in an email.
-# .*(\s\S+$) matches all remaining characters until the end, where it captures the user name.
-pattern = "(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}),\d{3} ([AU]).*(\s\S+$)"
+# (\S+$) matches all characters after the last whitespace until the end, where it captures the user name.
+pattern = "(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}),\d{3} ([AU]).*\s(\S+$)"
+
+# SMTP info. All fields required. SMTP over TLS on port 587 only; if you want it differently, code it yourself :P
+smtp_user = ""
+smtp_pass = ""
+smtp_server = ""
+smtp_sender = ""
+# this can be one email address or a list of email addresses
+smtp_recipients = ""
 
 def main(args=[]):
     # Can we convert the first argument passed in into a date? If not, use today's date.
@@ -61,9 +72,25 @@ def main(args=[]):
     except NameError:
         print("Unable to find file " + filename + ". Check your path and try again.", file=sys.stderr)
         
-    # print our CSV.
-    # TODO: replace with emailing this out? Or pipe to sendmail?
-    print(result)
+    # send out our CSV, unless we don't have all email creds or 'noemail' is in the program args, in which case print them
+    if smtp_server and smtp_user and smtp_pass and smtp_sender and smtp_recipients and not ("noemail" in args):
+        message = MIMEText(result)
+        message['Subject'] = "Latchscan results for " + target_date.isoformat()
+        message['From'] = smtp_sender
+        message['To'] = smtp_recipients
+        mail = smtplib.SMTP()
+        try:
+            mail.connect(smtp_server,587)
+            mail.starttls()
+            mail.login(smtp_user,smtp_pass)
+            mail.send_message(message)
+        except smtplib.SMTPException as e:
+            print("SMTP error: " + str(e), file=sys.stderr)
+        finally:
+            mail.quit()
+    else:
+        print(result)
+        
 
 if (len(sys.argv) >= 2):
     main(sys.argv[1:])
